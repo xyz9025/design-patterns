ab46d8f78813f459ae7ee78a38b4f2ddbe258c55
yangran <499615297@qq.com>
2019-03-03 10:09:30 +0800

    fix

--- src/main/java/design/template/Client.java
+++ src/main/java/design/template/Client.java
@@ -11,6 +11,20 @@ public class Client {
         cacheTemplate.put("a","Hello World");
         String str = cacheTemplate.get("a");
         System.out.println(str);
+
+        Integer i01 = 59;
+        int i02 = 59;
+        Integer i03 =Integer.valueOf(59);   //-128到127有缓存。是同一个对象
+        Integer i04 = new Integer(59);
+        Integer i05 = new Integer(59);
+
+        System.out.println(i01==i02);
+        System.out.println(i01==i03);
+        System.out.println(i01==i04);
+        System.out.println(i03==i04);
+        System.out.println(i02==i03);
+        System.out.println(i02==i04);
+        System.out.println(i03==i05);
     }
 
 }
